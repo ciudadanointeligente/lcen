702590f216822394402b81da97b8fe675d7543bc
pedregalux <pedregalux@users.noreply.github.com>
2022-08-11 15:53:37 -0400

    Delete evaluacionesinternacionales directory

--- evaluacionesinternacionales/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import EvaluadoraNormaInt,CategoriaNormaInt,TagNormaInt,NormaInt
-
-
-admin.site.register(EvaluadoraNormaInt)
-admin.site.register(CategoriaNormaInt)
-admin.site.register(TagNormaInt)
-admin.site.register(NormaInt)
